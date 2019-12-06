3fb47c1ff8b67f7cede07641a08f7c6e783bb446
marcusvfgaldino <mvgaldino60@gmail.com>
2019-12-06 18:23:37 -0300

    Init

---
+++ filter-price.js
@@ -0,0 +1,34 @@
+/* CAMPOS DE FILTRO DE PREÇO CUSTOMIZÁVEL NA PÁGINA DE CATEGORIAS 
+   AUTOR: Marcus Galdino
+   CONTATO: mvgaldino60@gmail.com
+*/
+        
+function showFilter(start = true, show = ".Faixa.preço", max = false) {
+        
+    content = "<input id='min' type='number' placeholder='Min.' required></input><input id='max' type='number' placeholder='Máx.' required></input><button id='go'>Enviar</button>"
+
+    if(start === true ) {
+        $(show).append(content);
+    } else if(start === false) {
+        $(show).prepend(content);
+    } else {
+        console.log("Erro. Parâmetro show deve ser TRUE ou FALSE");
+        return
+    }
+    
+    $("#go").click(function(max){
+
+        var valmin = $("#min").val();
+        var valmax = $("#max").val();
+           
+        if(max !== false) {
+            if(valmax > max) {
+                alert("Valor máximo deve ser menor que " + max);
+                return;
+            }
+        }
+        window.location.href = "de-" + valmin + "-a-" + valmax + "?PS=32&map=c,priceFrom";
+            
+    })
+}
+
